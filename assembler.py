24e95fd5c1ff3f505fd2fd864bd5f97a77732656
OmreFrauk <117479359+OmreFrauk@users.noreply.github.com>
2022-12-06 15:39:11 +0300

    register value to binary

--- assembler.py
+++ assembler.py
@@ -4,24 +4,39 @@ class Converter:
         self.binary_code = []
         self.hex_code = []
 
-        self.opcode_dict = {
-            'ADD': '00001',
-            'SUB': '00010'
-        }
+        self.opcode_dict = {"ADD": "00001", "SUB": "00010"}
 
     def convert(self):
         for instruction in self.instructions:
-            binary_code = ''
+            binary_code = ""
 
-            line = instruction.split(' ')
+            line = instruction.split(" ")
             operation = line[0]
             operation = self.opcode_dict[operation]
-            arguments = line[1].split(',')
+            binary_code += operation
+            arguments = line[1].split(",")
+            if (
+                operation == "ADD"
+                or operation == "SUB"
+                or operation == "AND"
+                or operation == "OR"
+                or operation == "XOR"
+            ):  # they have same form
+                dest = arguments[0]
+                src_1 = arguments[1]
+                src_2 = arguments[2]
 
-    def reg_to_binary(self, **kwargs):
+                registers = self.reg_to_binary(dest=dest, src_1=src_1, src_2=src_2)
+                binary_code += "".join(registers)
+
+    @staticmethod
+    def reg_to_binary(**kwargs):
         binary = []
         for i in kwargs.values():
-            register = int(i.split('R')[1])
+            register = int(i.split("R")[1])
 
             if register > 15:
                 print(f"There is no register such {register}\n")
+                exit(-1)
+            binary.append(str(bin(register))[2:].zfill(4))
+        return binary
--- test.py
+++ test.py
@@ -1,1 +1,10 @@
+import assembler
+from assembler import *
+def main():
+    assembler1 = Converter.reg_to_binary(dest="R11",src_1="R12",src_2="R13")
+    print(assembler1)
+if __name__ == '__main__':
+    main()
+
+    print('clear')
 
